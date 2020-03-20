cdfa956fbd0a78cc359da4a91ff84a77a0b2d503
a-lei-7 <qiyi.li@aol.com>
2020-03-20 17:38:43 +0800

    init

---
+++ server.js
@@ -0,0 +1,142 @@
+var http = require("http");
+var fs = require("fs");
+var url = require("url");
+var port = process.argv[2];
+
+if (!port) {
+  console.log("请指定端口号好不啦？\nnode server.js 8888 这样不会吗？");
+  process.exit(1);
+}
+
+var server = http.createServer(function(request, response) {
+  var parsedUrl = url.parse(request.url, true);
+  var pathWithQuery = request.url;
+  var queryString = "";
+  if (pathWithQuery.indexOf("?") >= 0) {
+    queryString = pathWithQuery.substring(pathWithQuery.indexOf("?"));
+  }
+  var path = parsedUrl.pathname;
+  var query = parsedUrl.query;
+  var method = request.method;
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log("有个傻子发请求过来啦！路径（带查询参数）为：" + pathWithQuery);
+  if (path === "/sign_in" && method === "POST") {
+    response.setHeader("Content-Type", "text/html;charset=utf-8");
+    //读数据库
+    const userArray = JSON.parse(fs.readFileSync("./db/users.json"));
+
+    const array = [];
+    request.on("data", chunk => {
+      array.push(chunk);
+    });
+    request.on("end", () => {
+      const string = Buffer.concat(array).toString();
+      const obj = JSON.parse(string); // 包含填写的 name password
+      const user = userArray.find(
+        user => user.name === obj.name && user.password === obj.password
+      );
+      if (user === undefined) {
+        response.statusCode = 400;
+        response.setHeader("content-type", "text/json;charset=utf-8");
+        response.end(`{"errorCode": 4001}`);
+      } else {
+        response.statusCode = 200;
+        response.setHeader("Set-Cookie", `user_id=${user.id}; HttpOnly`);
+        response.end();
+      }
+    });
+  } else if (path === "/home.html") {
+    const cookie = request.headers["cookie"];
+    let user_id;
+    try {
+      userId = cookie
+        .split(";")
+        .filter(s => s.indexOf("user_id=") >= 0)[0]
+        .split("=")[1];
+    } catch (error) {}
+    if (userId) {
+      const userArray = JSON.parse(fs.readFileSync("./db/users.json"));
+      const user = userArray.find(user => user.id.toString() === userId);
+      userArray.find(user.id, toString() === userId);
+      const homeHtml = fs.readFileSync("./public/home.html").toString();
+      let string;
+      if (user) {
+        const string = homeHtml
+          .replace("{{loginStatus}}", "已登录")
+          .replace("{{user.name}}", user.name);
+      } else {
+        const string = homeHtml
+          .replace("{{loginStatus}}", "未登录")
+          .replace("{{user.name}}", "");
+      }
+      response.write(string);
+    } else {
+      const homeHtml = fs.readFileSync("./public/home.html").toString();
+      const string = homeHtml
+        .replace("{{loginStatus}}", "未登录")
+        .replace("{{user.name}}", "");
+      response.write(string);
+    }
+  } else if (path === "/register" && method === "POST") {
+    response.setHeader("Content-Type", "text/html;charset=utf-8");
+    //读数据库
+    const userArray = JSON.parse(fs.readFileSync("./db/users.json"));
+
+    const array = [];
+    request.on("data", chunk => {
+      array.push(chunk);
+    });
+    request.on("end", () => {
+      const string = Buffer.concat(array).toString();
+      const obj = JSON.parse(string);
+      const lastUser = userArray[userArray.length - 1];
+      const newUser = {
+        // id 为最后一个用户的 id + 1
+        id: lastUser ? lastUser.id + 1 : 1,
+        name: obj.name,
+        password: obj.password
+      };
+      userArray.push(newUser);
+      fs.writeFileSync("./db/users.json", JSON.stringify(userArray));
+    });
+  } else {
+    response.statusCode = 200;
+    // 默认首页
+    const filePath = path === "/" ? "/index.html" : path;
+    const index = filePath.lastIndexOf(".");
+    // suffix 是后缀
+    const suffix = filePath.substring(index);
+    const fileTypes = {
+      ".html": "text/html",
+      ".css": "text/css",
+      ".js": "text/javascript",
+      ".png": "image/png",
+      ".jpg": "image/jpeg"
+    };
+    response.setHeader(
+      "Content-Type",
+      `${fileTypes[suffix] || "text/html"};charset=utf-8`
+    );
+    let content;
+    try {
+      content = fs.readFileSync(`./public${filePath}`);
+    } catch (error) {
+      content = "文件不存在";
+      response.statusCode = 404;
+    }
+    response.write(content);
+  }
+  response.end();
+
+  /******** 代码结束，下面不要看 ************/
+});
+
+server.listen(port);
+console.log(
+  "监听 " +
+    port +
+    " 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:" +
+    port
+);
